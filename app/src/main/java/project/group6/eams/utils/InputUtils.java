05e12e6f10fecf1a83dcba0ef24fc681ca0a7460
cyoun00 <cyoun065@uottawa.ca>
2024-10-07 14:40:48 -0400

    added isValidPassword method to InputUtils.java

--- app/src/main/java/project/group6/eams/utils/InputUtils.java
+++ app/src/main/java/project/group6/eams/utils/InputUtils.java
@@ -16,4 +16,55 @@ public class InputUtils {
         return (!TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches());
     }
 
+    /**
+     * Checks if a string matches the password pattern and isn't empty
+     * @param password string to check
+     * @return true if password is valid, false otherwise
+     */
+    public static String isValidPassword(String password){
+        if (!TextUtils.isEmpty(password)){
+            int numOfCapitals = 0;
+            int numOfLowerCase = 0;
+            int numOfSpecialChar = 0;
+            int numOfNum = 0;
+            String message = "Password must contain: ";
+            for (int i = 0; i<password.length(); i++){
+                char character = password.charAt(i);
+                if (Character.isUpperCase(character)){
+                    numOfCapitals++;
+                }
+                else if (Character.isLowerCase(character)){
+                    numOfLowerCase++;
+                }
+                else if (Character.isDigit(character)){
+                    numOfNum++;
+                }
+                else{
+                    numOfSpecialChar++;
+                }
+            }
+            if(numOfCapitals < 1) {
+                message = message + "\nAt least one upper case letter";
+            }
+            if(numOfLowerCase < 1) {
+                message = message + "\nAt least one lower case letter";
+            }
+            if(numOfNum < 1) {
+                message = message + "\nAt least one number";
+            }
+            if(numOfSpecialChar < 1) {
+                message = message + "\nAt least one special character. Eg: @,!,?";
+            }
+            if(password.length() < 8) {
+                message = message + "\nAt least 8 characters";
+            }
+            if(message.equals("Password must contain: ")) {
+                message = "Valid Password";
+            }
+            return message;
+        }
+        return "Password must contain: \nAt least one upper case letter \nAt least one lower case letter" +
+                "\nAt least one number \nAt least one special character. Eg: @,!,? \nAt least 8 characters";
+    }
+
 }
