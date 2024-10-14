b36b9db04bc1ef242aa1f0c385ee36691cc762c2
Brooke-8 <bmacq014@uottawa.ca>
2024-10-14 17:53:43 -0400

    added organizer & admin pages

---
+++ app/src/main/java/project/group6/eams/activities/OrganizerPage.java
@@ -0,0 +1,25 @@
+package project.group6.eams.activities;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import project.group6.eams.R;
+
+public class OrganizerPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_organizer_page);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
