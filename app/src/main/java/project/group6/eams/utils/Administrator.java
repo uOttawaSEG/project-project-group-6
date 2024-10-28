f8ab4ce96227a9393299666549f0cb71a730c21f
rlbeagley <rbeagleyuni@gmail.com>
2024-10-28 19:52:49 -0400

    changed same variable name

--- app/src/main/java/project/group6/eams/utils/Administrator.java
+++ app/src/main/java/project/group6/eams/utils/Administrator.java
@@ -20,14 +20,14 @@ public class Administrator extends User{
      * Allows administrator to reject a RegisterableUser (Organizer or Attendee) following their sign up attempt
      * by retrieving the user object using the email as the ID.
      *
-     * @param email of type String, user's ID
+     * @param emailID of type String, user's ID
      */
-    public void rejectRequest (String email) { // i can change this if u send email instead of RegisterableUser
-        registrationManager.changeUserStatus(email,false, new RegistrationManager.RegistrationCallback() {
+    public void rejectRequest (String emailID) { // i can change this if u send email instead of RegisterableUser
+        registrationManager.changeUserStatus(emailID,false, new RegistrationManager.RegistrationCallback() {
 
             @Override
             public void onSuccess() {
-                Log.d("Users", email + " rejected");
+                Log.d("Users", emailID + " rejected");
             }
 
             @Override
