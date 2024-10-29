8c4d91c23484e8d2db3351978a08aecc22a44f00
rlbeagley <rbeagleyuni@gmail.com>
2024-10-28 21:19:28 -0400

    fixed variable name

--- app/src/main/java/project/group6/eams/utils/RegistrationManager.java
+++ app/src/main/java/project/group6/eams/utils/RegistrationManager.java
@@ -133,7 +133,7 @@ public class RegistrationManager {
                     user.setRejectionStatus(true);
                 }
 
-                users.writeToReference(email.toLowerCase().replaceAll(" ",""),userDoc);
+                users.writeToReference(email.toLowerCase().replaceAll(" ",""),user);
             } catch (Exception e) {
                 Log.e("Database", Objects.requireNonNull(e.getMessage()));
                 callback.onError(e);
