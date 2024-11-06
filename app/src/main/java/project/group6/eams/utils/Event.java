ca2ec9653a6bf3684b520830fb16072b2f1ad076
cyoun00 <cyoun065@uottawa.ca>
2024-11-05 21:39:43 -0500

    created event class

---
+++ app/src/main/java/project/group6/eams/utils/Event.java
@@ -0,0 +1,43 @@
+package project.group6.eams.utils;
+
+import java.util.Date;
+
+public class Event {
+
+    public String title;
+    public String description;
+    public String eventAddress;
+    public Date startTime;
+    public Date endTime;
+    public boolean automaticApproval;
+    public User creator;
+
+    public Event(String title, String description, String eventAddress, Date startTime, Date endTime, boolean automaticApproval, User creator){
+        this.automaticApproval = automaticApproval;
+        this.creator = creator;
+        this.eventAddress = eventAddress;
+        this.endTime = endTime;
+        this.description =description;
+        this.startTime = startTime;
+        this.title = title;
+
+    }
+
+    //Getters
+    public boolean getAutomaticApproval() {return automaticApproval;}
+    public User getCreator() {return creator;}
+    public String getEventAddress(){return eventAddress;}
+    public Date getEndTime() {return endTime;}
+    public Date getStartTime() {return startTime;}
+    public String getDescription() {return description;}
+    public String getTitle() {return title;}
+
+    //Setters
+    public void setAutomaticApproval() {this.automaticApproval = automaticApproval;}
+    public void setCreator() {this.creator = creator;}
+    public void setEventAddress(){this.eventAddress = eventAddress;}
+    public void setEndTime() {this.endTime = endTime;}
+    public void setStartTime() {this.startTime = startTime;}
+    public void setDescription() {this.description =description;}
+    public void setTitle() {this.title = title;}
+}
