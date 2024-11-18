f98a534ab08d37485542c339d90c5b1eb6897235
Brooke-8 <bmacq014@uottawa.ca>
2024-11-18 14:00:08 -0500

    Did basic attendee page java

--- app/src/main/java/project/group6/eams/activities/AttendeePage.java
+++ app/src/main/java/project/group6/eams/activities/AttendeePage.java
@@ -1,7 +1,9 @@
 package project.group6.eams.activities;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -11,12 +13,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Objects;
 
 import project.group6.eams.R;
+import project.group6.eams.activityUtils.AttendeeEventAdapter;
+import project.group6.eams.activityUtils.EventAdapter;
+import project.group6.eams.utils.Event;
+import project.group6.eams.utils.EventManager;
 
 public class AttendeePage extends AppCompatActivity {
 
     private Button logOffButton;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
@@ -30,10 +42,13 @@ public class AttendeePage extends AppCompatActivity {
         });
         initViews();
         initListeners();
+
     }
 
     private void initViews () {
         logOffButton = findViewById(R.id.logOffButton);
+        recyclerView = findViewById(R.id.recycler_view_attendee);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
     }
     private void initListeners () {
@@ -45,6 +60,20 @@ public class AttendeePage extends AppCompatActivity {
                 Intent intent = new Intent(AttendeePage.this, MainActivity.class);
                 startActivity(intent);
 
+            }
+        });
+        EventManager eventManager = new EventManager("Events");
+        eventManager.getUpcomingEvents("Attendee", new EventManager.EventCallbackList() {
+            @Override
+            public void onSuccess(ArrayList<Event> eventList) {
+                Log.d("Events", eventList.toString());
+                AttendeeEventAdapter adapter = new AttendeeEventAdapter(eventList, false);
+                recyclerView.setAdapter(adapter);
+                adapter.notifyDataSetChanged();
+            }
+            @Override
+            public void onError(Exception e) {
+                Log.e("Database", Objects.requireNonNull(e.getMessage()));
             }
         });
     }
---
+++ app/src/main/java/project/group6/eams/activityUtils/AttendeeEventAdapter.java
@@ -0,0 +1,87 @@
+package project.group6.eams.activityUtils;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import project.group6.eams.R;
+import project.group6.eams.users.Attendee;
+import project.group6.eams.users.Organizer;
+import project.group6.eams.utils.Event;
+import project.group6.eams.utils.EventManager;
+
+public class AttendeeEventAdapter extends RecyclerView.Adapter<AttendeeEventAdapter.ViewHolder> {
+    private final ArrayList<Event> events;
+    private boolean isOnPastEventPage;
+
+    private RecyclerView recyclerViewAttendee;
+    Organizer organizer = new Organizer();
+
+    public AttendeeEventAdapter (ArrayList<Event> events, boolean isOnPastEventPage) {
+        this.events = events;
+        this.isOnPastEventPage = isOnPastEventPage;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView eventTitle;
+        public TextView creator;
+        public TextView startTime;
+        public TextView endTime;
+        public TextView eventAddress;
+        public TextView eventDescription;
+        public Button requestToAttendButton;
+        public Button delete;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            eventTitle = itemView.findViewById(R.id.event_title);
+            creator = itemView.findViewById(R.id.creator);
+            startTime = itemView.findViewById(R.id.startTime);
+            endTime = itemView.findViewById(R.id.endTime);
+            eventAddress = itemView.findViewById(R.id.eventAddress);
+            eventDescription = itemView.findViewById(R.id.description);
+            requestToAttendButton = itemView.findViewById(R.id.attendee_list_button);
+            delete = itemView.findViewById(R.id.delete_event);
+        }
+    }
+
+    @NonNull
+    @Override
+    public AttendeeEventAdapter.ViewHolder onCreateViewHolder (@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.eventlist_layout,
+                parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder (@NonNull AttendeeEventAdapter.ViewHolder holder, int position) {
+        Event event = events.get(position);
+        holder.eventTitle.setText(event.getTitle());
+        holder.creator.setText(event.getCreator().getEmail());
+        holder.eventAddress.setText(event.getEventAddress());
+        holder.eventDescription.setText(event.getDescription());
+        holder.startTime.setText(event.getStartTime().toString());
+        holder.endTime.setText(event.getEndTime().toString());
+        holder.requestToAttendButton.setText("Request");
+        holder.delete.setText("temp");
+        /*request to attend holder.requestToAttendButton.setOnClickListener();*/
+        /* prolly remove holder.delete.setOnClickListener(v -> organizer.deleteEvent(event));*/
+
+    }
+    @Override
+    public int getItemCount () {
+        return events.size();
+    }
+
+}
